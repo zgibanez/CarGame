18e64fc5a2a1dce973b5496e66e45a137aef1db1
rutgerkra <r.kraaijer@gmail.com>
2018-12-13 23:16:37 +0100

    added first version of bezier curves to path

--- CarGame/Assets/Path.cs
+++ CarGame/Assets/Path.cs
@@ -8,6 +8,8 @@ public class Path {
     public GameObject[] endCorners; 
     public Vector2[] midways;
 
+    Vector3[] keypoints;
+
     public float length;
 
     /// <summary>
@@ -19,15 +21,51 @@ public class Path {
     {
         this.startCorners = startCorners;
         this.endCorners = endCorners;
-        GetMidwaysAndLength();
+
+        keypoints = new Vector3[4]; // initialize once
+        UpdateKeypoints();
     }
 
-    public void GetMidwaysAndLength()
+    /// <summary>
+    /// function that can be used to recalculate the number and configurations of the keypoints of the path center
+    /// </summary>
+    public void UpdateKeypoints()
     {
-        midways = new Vector2[2];
-        midways[0] = (startCorners[0].transform.position + startCorners[1].transform.position) / 2;
-        midways[1] = (endCorners[0].transform.position + endCorners[1].transform.position) / 2;
-        length = Vector2.Distance(midways[0], midways[1]);
+        keypoints = new Vector3[4];
+
+        // set center points as end points
+        keypoints[0] = Vector3.Lerp(startCorners[0].transform.position, startCorners[1].transform.position, 0.5f);
+        keypoints[3] = Vector3.Lerp(endCorners[0].transform.position, endCorners[1].transform.position, 0.5f);
+
+        // determine 1/2 of length between end points (arbitrary factor. lower factor results in less sharp curve)
+        float keypointDistance = Vector3.Distance(keypoints[0], keypoints[3]) / 2f;
+
+        // add intermediate points normal to start and end points at distance calculated above
+        Vector3 startForwards = (startCorners[1].transform.position - keypoints[0]);
+        startForwards = Quaternion.AngleAxis(-90, Vector3.back) * startForwards;
+        startForwards = keypoints[0] + startForwards.normalized * keypointDistance;
+        keypoints[1] = startForwards;
+
+        Vector3 endBackwards = (endCorners[0].transform.position - keypoints[3]);
+        endBackwards = Quaternion.AngleAxis(90, Vector3.back) * endBackwards;
+        endBackwards = keypoints[3] + endBackwards.normalized * keypointDistance;
+        keypoints[2] = endBackwards;
+
+        CalculateLength();
+    }
+
+    void CalculateLength()
+    {
+        length = 0;
+        Vector3 pointA = keypoints[0];
+        Vector3 pointB;
+        int resolution = 10;
+        for (float i = 1; i < resolution; i++)
+        {
+            pointB = BezierPoint(keypoints, i / resolution);
+            length += Vector3.Distance(pointA, pointB);
+            pointA = pointB;
+        }
     }
 
     /// <summary>
@@ -36,10 +74,39 @@ public class Path {
     /// <param name="pathD"> Distance travelled along the road segment.</param>
     /// <param name="horiD"> Horizontal deviation from the midway path.</param>
     /// <returns></returns>
-    public virtual Vector2 GetPosition(float pathD, float horiD)
+    public virtual Vector3 GetPosition(float pathD, float horiD)
     {
-        float lambda = length / pathD; //Percentage of path traveled
-        return Vector2.Lerp(midways[0], midways[1], lambda);
+        return BezierPoint(keypoints, pathD / length);
     }
 
+    Vector3 BezierPoint(Vector3[] points, float t)
+    {
+        if (points.Length == 2) return Vector3.Lerp(points[0], points[1], t);
+        Vector3[] subPoints = new Vector3[points.Length - 1];
+        for (int i = 0; i < subPoints.Length; i++)
+        {
+            subPoints[i] = BezierPoint(new Vector3[] { points[i], points[i + 1] }, t);
+        }
+        return BezierPoint(subPoints, t);
+    }
+
+    public void DrawGizmo()
+    {
+        Gizmos.color = Color.red;
+
+        // draw keypoints
+        for (int i = 0; i < keypoints.Length; i++)
+            Gizmos.DrawSphere(keypoints[i], 0.05f);
+
+        // draw curve
+        Vector3 pointA = keypoints[0];
+        Vector3 pointB;
+        int resolution = 20;
+        for (float i = 1; i <= resolution; i++)
+        {
+            pointB = BezierPoint(keypoints, i / resolution);
+            Gizmos.DrawLine(pointA, pointB);
+            pointA = pointB;
+        }
+    }
 }
--- CarGame/Assets/Road.cs
+++ CarGame/Assets/Road.cs
@@ -7,14 +7,11 @@ public class Road : RoadSegment {
     //!!PROVISIONAL ONLY FOR VISUALIZATION!!
     private void Update()
     {
-        path.GetMidwaysAndLength();
+        path.UpdateKeypoints();
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(path.startCorners[0].transform.position, path.endCorners[1].transform.position);
-        Gizmos.DrawLine(path.startCorners[1].transform.position, path.endCorners[0].transform.position);
-        Gizmos.DrawLine(path.midways[0], path.midways[1]);
+        path.DrawGizmo();
     }
 }
