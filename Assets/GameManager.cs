dfe2872124253b6042cbe4bb2ca953112669a3c2
Ibáñez de Aldecoa Marin,Z.G. (Guillermo) <zoiletum@gmail.com>
2018-12-17 21:25:36 +0100

    Added GameManager prefab and changed script name
    GameManager controls the flow of the game. Prefab added in scenes.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameTester : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
     public IntersectionManager intersectionManager;
     public RoadSegment initialRoadSegment;
